02264521e4d17dd9cefbdaa63f76de74a4d464e8
lutfiqaraman <lutfiqaraman@gmail.com>
2020-06-05 23:48:42 +0300

    Create Account Controller

---
+++ backend/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AccountController : ControllerBase
+  {
+    private readonly AppDB _db;
+
+    public AccountController(AppDB db)
+    {
+        _db = db;
+    }
+  }
+}
